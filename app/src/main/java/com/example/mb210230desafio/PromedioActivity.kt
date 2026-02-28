aa7524011d90252bbdf7d81f06a2f5ba429c17d2
Emilyll000 <163612342+Emily000l@users.noreply.github.com>
2026-02-28 15:10:20 -0600

    aplicando cambios para ejercicio 1 y creando las activity de descuento de salario y calculadora

--- app/src/main/java/com/example/mb210230desafio/PromedioActivity.kt
+++ app/src/main/java/com/example/mb210230desafio/PromedioActivity.kt
@@ -8,41 +8,68 @@ import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.text.DecimalFormat
 
 class PromedioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_promedio)
-        val etNota1 = findViewById<EditText>(R.id.etNota1)
-        val etNota2 = findViewById<EditText>(R.id.etNota2)
-        val etNota3 = findViewById<EditText>(R.id.etNota3)
-        val etNota4 = findViewById<EditText>(R.id.etNota4)
-        val etNota5 = findViewById<EditText>(R.id.etNota5)
+        val editNombre = findViewById<EditText>(R.id.editNombre)
+        val editNota1 = findViewById<EditText>(R.id.editNota1)
+        val etNota2 = findViewById<EditText>(R.id.editNota2)
+        val etNota3 = findViewById<EditText>(R.id.editNota3)
+        val etNota4 = findViewById<EditText>(R.id.editNota4)
+        val etNota5 = findViewById<EditText>(R.id.editNota5)
         val btnCalcular = findViewById<Button>(R.id.btnCalcular)
-        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val tvResultado = findViewById<TextView>(R.id.textviewResultado)
+        val tvEstado = findViewById<TextView>(R.id.textviewEstado)
+
 
         btnCalcular.setOnClickListener {
 
-            val nota1 = etNota1.text.toString().toDoubleOrNull()
+            val nombre = editNombre.text.toString()
+
+            val nota1 = editNota1.text.toString().toDoubleOrNull()
             val nota2 = etNota2.text.toString().toDoubleOrNull()
             val nota3 = etNota3.text.toString().toDoubleOrNull()
             val nota4 = etNota4.text.toString().toDoubleOrNull()
             val nota5 = etNota5.text.toString().toDoubleOrNull()
 
-            if (nota1 != null && nota2 != null && nota3 != null && nota4 != null && nota5 != null) {
+            if (nombre.isNotEmpty() &&
+                nota1 != null && nota2 != null &&
+                nota3 != null && nota4 != null && nota5 != null) {
 
-                val promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
-                tvResultado.text = "Resultado: $promedio"
+                //validacion del rango, en lo del punto 5 o literal 5.
+                if (nota1 !in 0.0..10.0 ||
+                    nota2 !in 0.0..10.0 ||
+                    nota3 !in 0.0..10.0 ||
+                    nota4 !in 0.0..10.0 ||
+                    nota5 !in 0.0..10.0) {
 
-            } else {
-                tvResultado.text = "Ingrese valores válidos"
+                    tvResultado.text = "ERROR una/varias de las ntas no esta entre 0 y 10"
+                    tvEstado.text = ""
+                    return@setOnClickListener
+                }
+
+                val promedio =
+                    (nota1 * 0.15) +
+                            (nota2 * 0.15) +
+                            (nota3 * 0.20) +
+                            (nota4 * 0.25) +
+                            (nota5 * 0.25)
+
+                val df = DecimalFormat("#.00")
+                val promedioFormateado = df.format(promedio)
+
+                tvResultado.text = "Resultado: $promedioFormateado"
+
+                if (promedio >= 6.0) {
+                    tvEstado.text = "Aprobado"
+                } else {
+                    tvEstado.text = "Reprobado"
+                }
             }
-        }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 }
